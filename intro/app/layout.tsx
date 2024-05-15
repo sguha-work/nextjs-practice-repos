f108b07e93149392bc3d7983ad5afcd12e6e0ccb
sguha-work <sguha1988.life@gmail.com>
2024-05-15 22:54:42 +0530

    changes

--- intro/app/layout.tsx
+++ intro/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>My first next app</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
